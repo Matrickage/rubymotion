ae8abdb57c9470ee937e5b01883cc626f68fc722
lluis <luis.sancho@razorfishhealthware.com>
2014-02-17 13:09:22 +0100

    Models

--- UserProfile/app/app_delegate.rb
+++ UserProfile/app/app_delegate.rb
@@ -1,5 +1,13 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @user = User.new(id: "1", name: "Lluis", email: "lluissancho@mail.com", phone: "667233XXX")
+    @user_controller = UserController.alloc.initWithUser(@user)
+    @nav_controller =
+      UINavigationController.alloc.initWithRootViewController(@user_controller)
+    @window.rootViewController = @nav_controller
+    @window.makeKeyAndVisible
     true
+
   end
 end
